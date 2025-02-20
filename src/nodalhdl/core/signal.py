e6841ab33ce29b7fcec7f052a7a0a00ee74ebdc6
gralerfics <glverfer@outlook.com>
2025-02-20 12:46:25 +0800

    feat(signal): add Bundle, IOWrapper; add attribute isolator for Bundle.

--- src/nodalhdl/core/signal.py
+++ src/nodalhdl/core/signal.py
@@ -6,7 +6,8 @@
         UInt[<width>], UInt8, UInt16, UInt32, UInt64;
         SInt[<width>], SInt8, SInt16, SInt32, SInt64;
         FixedPoint[<integer_width>, <fraction_width>];
-        FloatingPoint[<exponent_width>, <fraction_width>], Float, Double.
+        FloatingPoint[<exponent_width>, <fraction_width>], Float, Double;
+        Bundle[<members>].
     Examples:
         a = UInt[8]()
         print(isinstance(a, UInt[8]))       # True
@@ -24,8 +25,8 @@
         print(isinstance(UInt, SignalType))                 # True (*)
         print(isinstance(UInt[8], SignalType))              # True
 
-        print(type(Signal))                                 # <class '__main__.SignalType'>
-        print(type(UInt[8]))                                # <class '__main__.BitsType'>
+        print(type(Signal))                                 # <class '...SignalType'>
+        print(type(UInt[8]))                                # <class '...BitsType'>
         print(issubclass(UInt[8], UInt))                    # True
         print(issubclass(UInt[8], BitsType))                # False
         print(issubclass(type(UInt[8]), BitsType))          # True
@@ -38,6 +39,15 @@
 
         print(UInt.determined)                              # False
         print(UInt[8].determined)                           # True
+        
+        S = Bundle[{
+            "a": UInt[8],
+            "b": UInt[8],
+            "c": Bit
+        }]
+        s = S()
+        print(S.T)                                          # {'a': <class '...UInt_8'>, 'b': <class '...UInt_8'>, 'c': <class '...Bits_1'>}
+        print(s.a)                                          # <...UInt_8 object at ...>
 +--------------------------------------------------------------------+
     Comments:
         1.  `Type` 为创建类型的类型，区分以表示不同类的创建格式，
@@ -54,8 +64,11 @@
             或可在某处实现一成员方法，用于给出两种类型间某运算的名称，以便后续生成对应的硬件模块文件的命名。
         4.  或许运算也应该定义为类，本身记录参与运算的类型和必要参数，不涉具体值，用于构建运算结构。
             也便于实现 (3.) 中的最后一句话。仿真运算的实现也可置于其中，解耦两个文件。
+        5.  TODO 实例需不需要能获取到类属性?
 +--------------------------------------------------------------------+
 """
+
+import hashlib
 
 
 """ Exceptions """
@@ -64,7 +77,7 @@ class SignalTypeException(Exception): pass
 
 """ Metatypes """
 class SignalType(type):
-    def __call__(self, *args, **kwds): # TODO
+    def __call__(self, *args, **kwds):
         if not self.determined:
             raise TypeError("Undetermined types cannot be instantiated.")
         else:
@@ -75,7 +88,7 @@ class SignalType(type):
     def instantiate_type(cls, new_type_name, properties = {}): # `cls` here is the generated class, type(cls) is SignalType or its subclasses
         if not new_type_name in cls.type_pool.keys():
             cls.type_pool[new_type_name] = type(f"{new_type_name}", (cls, ), {
-                "determined": True, # all generated types has the attribute `determined`
+                "determined": True, # guarantee that all types that are correctly generated have the attribute `determined: True`
                 **properties
             })
         return cls.type_pool.get(new_type_name)
@@ -123,6 +136,37 @@ class FloatingPointType(BitsType):
         else:
             raise SignalTypeException(f"Invalid parameter(s) \'{item}\' for type {cls.__name__}[<exponent_width (int)>, <fraction_width (int)>]")
 
+class IOWrapperType(SignalType):
+    def __getitem__(cls, item):
+        if isinstance(item, SignalType):
+            # TODO: 限制不能 IOWrapper 套 IOWrapper (注意可能存在 Bundle 下的递归结构)
+            return cls.instantiate_type(
+                f"{cls.__name__}_{item.__name__}",
+                {
+                    "T": item
+                }
+            )
+        else:
+            raise SignalTypeException(f"Invalid parameter(s) \'{item}\' for type {cls.__name__}[<signal_type (SignalType)>]")
+
+class BundleType(SignalType):
+    def __getitem__(cls, item):
+        def __is_all_signal_type(item: dict):
+            for x in item.values():
+                if not isinstance(x, SignalType):
+                    return False
+            return True
+        
+        if isinstance(item, dict) and __is_all_signal_type(item):
+            return cls.instantiate_type(
+                f"{cls.__name__}_{hashlib.md5(str(item).encode('utf-8')).hexdigest()}",
+                {
+                    "T": item
+                }
+            )
+        else:
+            raise SignalTypeException(f"Invalid parameter(s) \'{item}\' for type {cls.__name__}[<members (dict)>]")
+
 
 """ Types """
 class Signal(metaclass = SignalType):
@@ -157,4 +201,23 @@ class FixedPoint(Bits, metaclass = FixedPointType): pass
 class FloatingPoint(Bits, metaclass = FloatingPointType): pass
 Float = FloatingPoint[8, 23]
 Double = FloatingPoint[11, 52]
+
+class Input(Signal, metaclass = IOWrapperType): pass
+class Output(Signal, metaclass = IOWrapperType): pass
+
+class Bundle(Signal, metaclass = BundleType):
+    def __init__(self):
+        super().__init__()
+        bundle_types = type(self).T
+        for key, T in bundle_types.items():
+            setattr(self, key, T())
+    
+    def __getattribute__(self, name): # TODO 将该功能包装为一个装饰器吧, 起名关于屏蔽/隔离对象对类属性的访问
+        """
+            若 Bundle 对象不包含恰好名为 T 的信号, 而用户希望访问保存信号类型的类属性 T, 则建议其使用 `type(...).T`.
+            若 Bundle 对象下恰好有名为 T 的信号, 则 object.T 访问的就是该信号的对象, 覆盖了对类属性 T 的访问.
+        """
+        if name == "T" and "T" not in self.__dict__.keys(): # type(self).T.keys():
+            raise AttributeError(f"\'type(object).T\' or \'s.__class__.T\' is recommended if you want to access the class attribute \'T\'")
+        return super().__getattribute__(name)
 
