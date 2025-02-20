e409d04e69def85abccca6cb0c260b80e2657152
gralerfics <glverfer@outlook.com>
2025-02-20 13:04:56 +0800

    feat(signal): encapsulate the class attribute issolator into a decorator.

--- src/nodalhdl/core/signal.py
+++ src/nodalhdl/core/signal.py
@@ -42,12 +42,21 @@
         
         S = Bundle[{
             "a": UInt[8],
-            "b": UInt[8],
-            "c": Bit
+            "b": Bit,
+            "c": Bundle[{
+                "x": SInt[3],
+                "y": SInt[5]
+            }],
+            "T": Float
         }]
         s = S()
-        print(S.T)                                          # {'a': <class '...UInt_8'>, 'b': <class '...UInt_8'>, 'c': <class '...Bits_1'>}
         print(s.a)                                          # <...UInt_8 object at ...>
+        print(s.c)                                          # <...Bundle_0cbbdef73dff1f7318ab831e99b5216d object at ...>
+        print(s.c.x)                                        # <...SInt_3 object at ...>
+        print(s.T)                                          # <...FloatingPoint_8_23 object at ...>
+        print(S.T)                                          # {'a': <class '...UInt_8'>, 'b': <class '...Bits_1'>, 'c': <class '...Bundle_0cbbdef73dff1f7318ab831e99b5216d'>, 'T': <class '...FloatingPoint_8_23'>}
+        print(S.T["c"].T)                                   # {'x': <class '...SInt_3'>, 'y': <class '...SInt_5'>}
+        print(s.c.T)                                        # AttributeError: 'type(object).T' or 'object.__class__.T' is recommended if you want to access the class attribute 'T'
 +--------------------------------------------------------------------+
     Comments:
         1.  `Type` 为创建类型的类型，区分以表示不同类的创建格式，
@@ -64,7 +73,6 @@
             或可在某处实现一成员方法，用于给出两种类型间某运算的名称，以便后续生成对应的硬件模块文件的命名。
         4.  或许运算也应该定义为类，本身记录参与运算的类型和必要参数，不涉具体值，用于构建运算结构。
             也便于实现 (3.) 中的最后一句话。仿真运算的实现也可置于其中，解耦两个文件。
-        5.  TODO 实例需不需要能获取到类属性?
 +--------------------------------------------------------------------+
 """
 
@@ -168,6 +176,28 @@ class BundleType(SignalType):
             raise SignalTypeException(f"Invalid parameter(s) \'{item}\' for type {cls.__name__}[<members (dict)>]")
 
 
+""" Decorators """
+def class_attribute_isolator(attrs: list[str]):
+    def decorator(cls):
+        """
+            类装饰器 attribute_isolator, 置于类前表明该类的部分类属性需要隔离 (由隔离列表 attrs 规定), 即:
+                (1.) 若对象 obj 自己不包含名为 <x> 的属性, 而用户希望访问类属性 <x>, 则建议其使用 `type(obj).<x>` 或 `obj.__class__.<x>`.
+                (2.) 若对象 obj 恰好拥有名为 <x> 的属性, 则 obj.<x> 访问的就是 obj 对象自身的 <x> 属性, 覆盖对类属性 <x> 的访问.
+            其实现为覆写 __getattribute__ 方法, 访问时检查 name 是否在 attrs 中;
+            同时, self.__dict__ 中只包含对象本身的属性, 不包含类属性, 故可以用于检查对象本身是否覆盖了该属性.
+        """
+        def new_getattribute(self, name):
+            if name in attrs and name not in self.__dict__.keys(): # 访问的属性名在隔离列表中, 且对象自己不包含该属性
+                raise AttributeError(f"\'type(object).{name}\' or \'object.__class__.{name}\' is recommended if you want to access the class attribute \'{name}\'")
+            return super(cls, self).__getattribute__(name) # TODO
+        
+        cls.__getattribute__ = new_getattribute
+        
+        return cls
+    
+    return decorator
+
+
 """ Types """
 class Signal(metaclass = SignalType):
     determined = False # i.e. width-determined w.r.t. signals
@@ -205,19 +235,11 @@ Double = FloatingPoint[11, 52]
 class Input(Signal, metaclass = IOWrapperType): pass
 class Output(Signal, metaclass = IOWrapperType): pass
 
+@class_attribute_isolator(["T"])
 class Bundle(Signal, metaclass = BundleType):
-    def __init__(self):
+    def __init__(self): # 递归式地实例化内部信号
         super().__init__()
         bundle_types = type(self).T
         for key, T in bundle_types.items():
             setattr(self, key, T())
-    
-    def __getattribute__(self, name): # TODO 将该功能包装为一个装饰器吧, 起名关于屏蔽/隔离对象对类属性的访问
-        """
-            若 Bundle 对象不包含恰好名为 T 的信号, 而用户希望访问保存信号类型的类属性 T, 则建议其使用 `type(...).T`.
-            若 Bundle 对象下恰好有名为 T 的信号, 则 object.T 访问的就是该信号的对象, 覆盖了对类属性 T 的访问.
-        """
-        if name == "T" and "T" not in self.__dict__.keys(): # type(self).T.keys():
-            raise AttributeError(f"\'type(object).T\' or \'s.__class__.T\' is recommended if you want to access the class attribute \'T\'")
-        return super().__getattribute__(name)
 
