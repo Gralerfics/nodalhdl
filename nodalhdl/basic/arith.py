5af40036aa07771efb593846b71c964a7caa02d9
gralerfics <glverfer@outlook.com>
2025-03-17 03:08:01 +0800

    feat(arith): update generation() for Addition.

--- nodalhdl/basic/arith.py
+++ nodalhdl/basic/arith.py
@@ -1,18 +1,11 @@
 from ..core.signal import SignalType, UInt, SInt, Input, Output, Auto, Bundle
-from ..core.structure import Structure, StructuralNodes, RuntimeId, StructureGenerationException, IOProxy
+from ..core.structure import Structure, RuntimeId, StructureGenerationException, IOProxy
 from ..core.hdl import HDLFileModel
 
 
 def Addition(op1_type: SignalType, op2_type: SignalType, fixed_id: str = None) -> Structure:
-    # 参数合法性检查 (结构性检查, 即不影响结构生成即可, 因为这里可能涉及到 Auto 等未推导的父类型, 导就完了. 行为性检查在 HDL 生成或仿真中进行)
-    
     # 创建结构
     res = Structure("addition", fixed_id = fixed_id)
-    """
-        此处命名 addition.
-        若生成时 originally determined, 则得名称 addition_{id}, 这个 id 是独一无二的 structure_id (传入不同参数到 Diagram 生成的结构是不同 id 的), 不会冲突;
-        若生成时 originally undetermined, 则名称前还会有前缀信息, 包含 inst_name, 不会冲突.
-    """
     
     # 声明 IO Ports
     res.add_port("op1", Input[op1_type])
@@ -30,33 +23,18 @@ def Addition(op1_type: SignalType, op2_type: SignalType, fixed_id: str = None) -
             io.op1.update(tr.base[tr.W])
     
     def generation(h: HDLFileModel, io: IOProxy):
-        op1_type, op2_type, res_type = io.op1.type, io.op2.type, io.res.type
+        t1, t2, tr = io.op1.type, io.op2.type, io.res.type
         
-        if not (op1_type.belongs(UInt) and op2_type.belongs(UInt) or op1_type.belongs(SInt) and op2_type.belongs(SInt)):
+        if not (t1.belongs(UInt) and t2.belongs(UInt) or t1.belongs(SInt) and t2.belongs(SInt)):
             raise StructureGenerationException(f"Only accept UInt + UInt or SInt + SInt")
-        if not res_type.W == max(op1_type.W, op2_type.W):
+
+        if not tr.W == max(t1.W, t2.W):
             raise StructureGenerationException(f"Result width should be the maximum of the two operands")
         
-        h.set_raw(f".vhd", \
-f"""\
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
-entity {h.entity_name} is
-    port (
-        op1: in std_logic_vector({op1_type.W - 1} downto 0);
-        op2: in std_logic_vector({op2_type.W - 1} downto 0);
-        res: out std_logic_vector({res_type.W - 1} downto 0)
-    );
-end entity;
-
-architecture Behavioral of {h.entity_name} is
-begin
-    res <= std_logic_vector(signed(op1) + signed(op2));
-end architecture;
-"""
-        )
+        if tr.belongs(UInt):
+            h.add_assignment("res", "std_logic_vector(unsigned(op1) + unsigned(op2))")
+        else:
+            h.add_assignment("res", "std_logic_vector(signed(op1) + signed(op2))")
     
     res.custom_deduction = deduction
     res.custom_generation = generation
