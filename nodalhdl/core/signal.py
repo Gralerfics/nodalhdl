1e5773467e767eccb96f884f85d3624adb77f3f8
gralerfics <glverfer@outlook.com>
2025-05-16 15:50:10 +0800

    bkup.

--- nodalhdl/core/signal.py
+++ nodalhdl/core/signal.py
@@ -1,50 +1,20 @@
-from typing import Dict
-
 import hashlib
 
-
-# """ Pool """
-# class SignalTypePoolException(Exception): pass
-
-# class SignalTypePool:
-#     pool: Dict[str, 'SignalType'] = {}
-    
-#     @staticmethod
-#     def fetch(uid: str):
-#         """
-#             fetch the structure object by unique_name, or None when not exists.
-#             sweep the invalid references (lazy tag).
-#         """
-#         s = ReusablePool.pool.get(unique_name, None)
-#         if s is not None:
-#             if s.is_reusable and s.unique_name == unique_name:
-#                 # exists and still valid
-#                 return s
-#             else:
-#                 # exists but invalid, clear
-#                 del ReusablePool.pool[unique_name]
-#                 return None
-#         else:
-#             return None
-    
-#     @staticmethod
-#     def register(s: 'Structure'):
-#         """
-#             register an structure into the pool.
-#             if not reusable then return False and nothing will be done.
-#         """
-#         if s.is_reusable:
-#             fetch_s = ReusablePool.fetch(s.unique_name)
-#             if fetch_s is not None and fetch_s != s: # exists and conflicts
-#                 raise ReusablePoolException("unique_name should be unique")
-#             else: # allowed
-#                 ReusablePool.pool[s.unique_name] = s
-#         else:
-#             return False
+from typing import Dict, Union
 
 
+""" SignalType """
 class SignalTypeException(Exception): pass
 class SignalTypeInstantiationException(Exception): pass
+
+_info_abbreviation_mapping = {
+    "W": "width",
+    "W_int": "width_integer",
+    "W_frac": "width_fraction",
+    "W_exp": "width_exponent",
+    "W_mant": "width_mantissa",
+    "T": "wrapped_type"
+}
 
 class SignalType:
     """
@@ -58,11 +28,15 @@ class SignalType:
             item = [item]
         return self.derive(*item)
     
+    def __getattr__(self, name): # __getattr__ is only called when the property not exists; __getattribute__ is called every time
+        name = _info_abbreviation_mapping.get(name, name)
+        return self.info.get(name, None)
+    
     def __repr__(self):
-        return self.literal()
+        return self.validal()
     
     """ @override """
-    def __call__(self, *args, **kwds): # -> 'SignalValue': # type instantiation
+    def __call__(self, *args, **kwds) -> 'SignalValue': # type instantiation
         raise SignalTypeInstantiationException(f"{self.base_name} cannot be instantiated")
     
     """ @override """
@@ -70,7 +44,7 @@ class SignalType:
         raise SignalTypeException(f"{self.__name__} has no derived types")
     
     """ @override """
-    def literal(self) -> str: # valid string
+    def validal(self) -> str: # valid string
         return "Signal"
     
     @property
@@ -78,12 +52,19 @@ class SignalType:
         return self.__class__
     
     @property
-    def base_name(self): # "xxxType" -> "xxx"
+    def base_name(self) -> str: # "xxxType" -> "xxx"
         return self.base.__name__[:-4]
     
     @property
-    def is_determined(self): # width-determined
-        return "width" in self.info.keys()
+    def is_determined(self) -> bool: # width-determined or subtype determined
+        if self.belong(Bits):
+            return "width" in self.info.keys()
+        elif self.belong(Bundle):
+            bundle_types: Dict[str, SignalType] = self.info.get("bundle_types", {})
+            return all([t.is_determined for t in bundle_types.values()])
+        elif self.belong(IOWrapper):
+            wrapped_type: SignalType = self.info.get("wrapped_type", None)
+            return wrapped_type is not None and wrapped_type.is_determined
     
     def _info_check(self, *keys):
         for key in keys:
@@ -94,38 +75,46 @@ class SignalType:
     def uid(self) -> str: # consistent hash of (base, info), for comparison
         raise NotImplementedError
     
+    def belong(self, other: 'SignalType') -> bool:
+        self_norm = eval(self.base.__name__) # TODO to be checked
+        other_norm = eval(other.base.__name__)
+        return issubclass(self_norm, other_norm)
+    
     @staticmethod
-    def _merge_base(base_0: type, base_1: type) -> type:
+    def _merge_base(base_0: type, base_1: type):
         raise NotImplementedError
     
     def merge(self, other: 'SignalType') -> 'SignalType':
         raise NotImplementedError # TODO 冲突在此判断; 返回的类型需要特化
 
 
-class BitsType(SignalType):
+class AutoType(SignalType):
+    pass
+
+
+class BitsType(AutoType):
     def __call__(self, *args, **kwds):
         self._info_check("width")
-        
-        raise NotImplementedError
+        return BitsValue(self, *args, **kwds)
     
     def derive(self, width: int) -> 'SignalType':
         assert isinstance(width, int)
         return BitsType({"width": width})
     
-    def literal(self) -> str:
+    def validal(self) -> str:
         width = self.info.get("width", None)
-        return f"Bits_{width}" if width is not None else "Bits"
+        return f"{self.base_name}_{width}" if width is not None else "Bits"
 
 
 class FixedPointType(BitsType):
     def __call__(self, *args, **kwds):
         raise SignalTypeInstantiationException(f"{self.base_name} cannot be instantiated")
     
-    def derive(self, width_integer: int, width_fraction: int = None) -> 'SignalType':
+    def derive(self, width_integer: int, width_fraction: int) -> 'SignalType':
         assert isinstance(width_integer, int) and isinstance(width_fraction, int)
         return FixedPointType({"width_integer": width_integer, "width_fraction": width_fraction})
     
-    def literal(self) -> str:
+    def validal(self) -> str:
         width_integer = self.info.get("width_integer", None)
         width_fraction = self.info.get("width_fraction", None)
         return f"{self.base_name}_{width_integer}_{width_fraction}" if width_integer is not None and width_fraction is not None else self.base_name
@@ -134,10 +123,9 @@ class FixedPointType(BitsType):
 class UFixedPointType(FixedPointType):
     def __call__(self, *args, **kwds):
         self._info_check("width_integer", "width_fraction")
-        
-        raise NotImplementedError
+        return UFixedPointValue(self, *args, **kwds)
 
-    def derive(self, width_integer: int, width_fraction: int = None) -> 'SignalType':
+    def derive(self, width_integer: int, width_fraction: int) -> 'SignalType':
         assert isinstance(width_integer, int) and isinstance(width_fraction, int)
         return UFixedPointType({"width_integer": width_integer, "width_fraction": width_fraction, "width": width_integer + width_fraction})
 
@@ -145,41 +133,190 @@ class UFixedPointType(FixedPointType):
 class SFixedPointType(FixedPointType):
     def __call__(self, *args, **kwds):
         self._info_check("width_integer", "width_fraction")
-        
-        raise NotImplementedError
+        return SFixedPointValue(self, *args, **kwds)
 
-    def derive(self, width_integer: int, width_fraction: int = None) -> 'SignalType':
+    def derive(self, width_integer: int, width_fraction: int) -> 'SignalType':
         assert isinstance(width_integer, int) and isinstance(width_fraction, int)
         return SFixedPointType({"width_integer": width_integer, "width_fraction": width_fraction, "width": 1 + width_integer + width_fraction})
+
+
+class IntegerType(FixedPointType):
+    def derive(self, width: int) -> 'SignalType':
+        assert isinstance(width, int)
+        return self.base({"width": width})
+    
+    def validal(self):
+        width = self.info.get("width", None)
+        return f"{self.base_name}_{width}" if width is not None else self.base_name
+
+
+class UIntType(IntegerType):
+    def __call__(self, *args, **kwds):
+        self._info_check("width")
+        return UIntValue(self, *args, **kwds)
+
+
+class SIntType(IntegerType):
+    def __call__(self, *args, **kwds):
+        self._info_check("width")
+        return SIntValue(self, *args, **kwds)
 
 
 class FloatingPointType(BitsType):
     def __call__(self, *args, **kwds):
         self._info_check("width_exponent", "width_mantissa")
-        
-        raise NotImplementedError
+        return FloatingPointValue(self, *args, **kwds)
     
-    def derive(self, width_exponent: int, width_mantissa: int = None) -> 'SignalType':
+    def derive(self, width_exponent: int, width_mantissa: int) -> 'SignalType':
         assert isinstance(width_exponent, int) and isinstance(width_mantissa, int)
         return FixedPointType({"width_exponent": width_exponent, "width_mantissa": width_mantissa})
     
-    def literal(self) -> str:
+    def validal(self) -> str:
         width_exponent = self.info.get("width_exponent", None)
         width_mantissa = self.info.get("width_mantissa", None)
         return f"{self.base_name}_{width_exponent}_{width_mantissa}" if width_exponent is not None and width_mantissa is not None else self.base_name
 
 
+class BundleType(AutoType):
+    def __call__(self, *args, **kwds):
+        self._info_check("bundle_types")
+        return BundleValue(self, *args, **kwds)
+    
+    def derive(self, bundle_types: Dict[str, SignalType]) -> 'SignalType':
+        assert isinstance(bundle_types, SignalType)
+        return BundleType({"bundle_types": bundle_types})
+    
+    def validal(self) -> str:
+        bundle_types: Dict[str, SignalType] = self.info.get("bundle_types", {})
+        return hashlib.md5(str(bundle_types).encode('utf-8')).hexdigest()
+
+
+class IOWrapperType(SignalType):
+    def __call__(self, *args, **kwds):
+        raise SignalTypeInstantiationException(f"{self.base_name} cannot be instantiated")
+    
+    def derive(self, wrapped_type: SignalType) -> 'SignalType':
+        assert isinstance(wrapped_type, SignalType)
+        return IOWrapperType({"wrapped_type": wrapped_type})
+    
+    def validal(self) -> str:
+        wrapped_type: SignalType = self.info.get("wrapped_type", None)
+        return f"{self.base_name}_{wrapped_type}" if wrapped_type is not None else self.base_name
+
+
 Signal = SignalType()
-
-Auto = SignalType()
-
+Auto = AutoType()
 Bits = BitsType()
-
 UFixedPoint = UFixedPointType()
-
 SFixedPoint = SFixedPointType()
-
+UInt = UIntType()
+SInt = SIntType()
 FloatingPoint = FloatingPointType()
+Bundle = BundleType()
+IOWrapper = IOWrapperType()
+
+
+""" SignalValue """
+class SignalValueException(Exception): pass
+
+class SignalValue:
+    def __init__(self, signal_type: SignalType, literal_value):
+        assert signal_type.base_name == self.__class__.__name__[:-5]
+        self.type = signal_type
+        self.set_internal(literal_value)
+    
+    def __getattr__(self, name):
+        return self.type.__getattr__(name)
+    
+    def __repr__(self):
+        return self.validal()
+    
+    @property
+    def literal(self):
+        return self.internal_to_literal(self.internal)
+    
+    def set_internal(self, literal_value):
+        self.internal = self.literal_to_internal(literal_value)
+
+    """ @override """
+    def literal_to_internal(self, literal_value):
+        return None
+    
+    """ @override """
+    def internal_to_literal(self, internal_value):
+        return None
+    
+    """ @override """
+    def validal(self) -> str:
+        raise SignalValueException(f"{self.type.base_name} cannot be converted to a valid representation")
+    
+    """ @override """
+    def to_bits_string(self):
+        raise SignalValueException(f"{self.type.base_name} cannot be converted to a bits-string")
+
+    """ @override """
+    # def __add__(self, other): ...
+    # ...
+
+
+class BitsValue(SignalValue):
+    def literal_to_internal(self, literal_value: Union[str, int]):
+        normalize = lambda x: x % (1 << self.W)
+        if isinstance(literal_value, int):
+            return normalize(literal_value)
+        elif isinstance(literal_value, str):
+            return int(literal_value[-self.W:], base = 2)
+        else:
+            raise NotImplementedError
+    
+    def internal_to_literal(self, internal_value):
+        return internal_value
+    
+    def validal(self) -> str:
+        return "b" + self.to_bits_string() # in default
+    
+    def to_bits_string(self):
+        return bin(self.internal)[2:].zfill(self.W)[-self.W:]
+
+
+class UFixedPointValue(BitsValue):
+    pass # TODO
+
+
+class SFixedPointValue(BitsValue):
+    def literal_to_internal(self, literal_value: float):
+        if isinstance(literal_value, float):
+            value_int = int(literal_value * (1 << self.W_frac))
+            half = 1 << (self.W - 1)
+            return (value_int + half) % (1 << self.W) - half
+        else:
+            raise NotImplementedError
+    
+    def internal_to_literal(self, internal_value):
+        return internal_value / (1 << self.W_frac)
+    
+    def validal(self) -> str:
+        return str(self.literal)
+    
+    def to_bits_string(self):
+        num_unsigned = self.internal + (1 << self.W) if self.internal < 0 else self.internal
+        return bin(num_unsigned)[2:].zfill(self.W)[-self.W:]
+
+
+class UIntValue(UFixedPointValue):
+    pass # TODO
+
+
+class SIntValue(SFixedPointValue):
+    pass # TODO
+
+
+class FloatingPointValue(BitsValue):
+    pass # TODO
+
+
+class BundleValue(SignalValue):
+    pass # TODO
 
 
 import sys
@@ -190,6 +327,10 @@ __all__ = [name for name in dir() if not name.startswith('_') and getattr(getatt
 print(Bits)
 print(Bits[8])
 print(SFixedPoint[16, 12])
+print(SInt[8])
+
+print(SFixedPoint[1, 1](1.5))
+print(SFixedPoint[1, 1](2.5))
 
 
 """
